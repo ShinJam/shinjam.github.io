099a9a7f5f1b05a1afeff4e401192c372cc15a8d
jam <nevvjann@gmail.com>
2020-05-27 14:06:03 +0900

    move browser-upgrade.html to head.html, delete fastclick

--- _assets/ts/search.ts
+++ _assets/ts/search.ts
@@ -1,55 +1,55 @@
 import algoliasearch from "algoliasearch/lite";
 import instantsearch from "instantsearch.js";
 import {
-    tags,
-    categories,
-    clearFilters,
-    clearFiltersEmptyResults,
-    clearFiltersMobile,
-    configuration,
-    hitsPerPage,
-    pagination,
-    dateSlider,
-    products,
-    resultsNumberMobile,
-    saveFiltersMobile,
-    searchBox,
-    sortBy,
+  tags,
+  categories,
+  clearFilters,
+  clearFiltersEmptyResults,
+  clearFiltersMobile,
+  configuration,
+  hitsPerPage,
+  pagination,
+  dateSlider,
+  products,
+  resultsNumberMobile,
+  saveFiltersMobile,
+  searchBox,
+  sortBy,
 } from "./widgets";
 import getRouting from "./routing";
 // import credential from "./credential";
 const credential = {
-    'application_id': '1YLEN0QM8Z',
-    'search_key': '9b6553e7658d9ff15e97dad1e7370ab5',
-    'index_name': 'dev_shinjam'
-  }
-  
+  application_id: "1YLEN0QM8Z",
+  search_key: "9b6553e7658d9ff15e97dad1e7370ab5",
+  index_name: "dev_shinjam",
+};
+
 const searchClient = algoliasearch(
-    credential.application_id,
-    credential.search_key
+  credential.application_id,
+  credential.search_key
 );
 
 const search = instantsearch({
-    indexName: credential.index_name,
-    searchClient,
-    routing: getRouting({ indexName: credential.index_name }),
+  indexName: credential.index_name,
+  searchClient,
+  routing: getRouting({ indexName: credential.index_name }),
 });
 
 search.addWidgets([
-    tags,
-    categories,
-    clearFilters,
-    clearFiltersEmptyResults,
-    clearFiltersMobile,
-    configuration,
-    hitsPerPage,
-    pagination,
-    dateSlider,
-    products,
-    resultsNumberMobile,
-    saveFiltersMobile,
-    searchBox,
-    sortBy,
+  tags,
+  categories,
+  clearFilters,
+  clearFiltersEmptyResults,
+  clearFiltersMobile,
+  configuration,
+  hitsPerPage,
+  pagination,
+  dateSlider,
+  products,
+  resultsNumberMobile,
+  saveFiltersMobile,
+  searchBox,
+  sortBy,
 ]);
 
 export default search;
