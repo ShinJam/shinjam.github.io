52b39c4e9f697b5a915a5be566b0097ebdf70b48
shinjam <nevvjann@gmail.com>
2020-06-01 00:02:02 +0900

    Init gatsby

--- _assets/ts/credential.ts
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const yaml = require("js-yaml");
-
-let fileContents = fs.readFileSync("./_config.yml", "utf8");
-let data = yaml.safeLoad(fileContents);
-let credential = data.algolia;
-
-export default credential;
