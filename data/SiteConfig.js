7ea548a60edd88e18d4edc7d58e1975f7002ecc6
shinjam <nevvjann@gmail.com>
2020-06-01 22:13:29 +0900

    Add GA id, disqus name

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -10,7 +10,7 @@ const config = {
   siteRssTitle: "Shinjam's development study logs RSS feed", // Title of the RSS feed
   siteFBAppID: "1825356251115265", // FB Application ID for using app insights
   googleAnalyticsID: "UA-166873913-1", // GA tracking ID.
-  disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter", // Disqus shortname.
+  disqusShortname: "shinjam-devlog", // Disqus shortname.
   dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
   dateFormat: "DD/MM/YYYY", // Date format for display.
   postsPerPage: 5, // Amount of posts displayed per listing page.
