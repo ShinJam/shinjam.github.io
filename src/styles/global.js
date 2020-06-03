ff8c5ba2c9ef95cd1de4de469ca4e59f34b6ab04
shinjam <nevvjann@gmail.com>
2020-06-03 23:23:44 +0900

    Edit: global css https://jbee.io/etc/intro-new-blog/#styling

--- src/styles/global.js
+++ src/styles/global.js
@@ -32,6 +32,13 @@ const globalStyles = css`
                 color: white;
             }
         }
+        
+        text-shadow: 0 0 0.1px rgba(0, 0, 0, 0.3);
+        -webkit-text-size-adjust: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        -moz-font-smoothing: antialiased;
+        text-rendering: optimizelegibility;
     }
 
     /*
