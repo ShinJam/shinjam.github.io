61c07d51c4cc46ba42cdcf926352f020a459dbbf
jam <nevvjann@gmail.com>
2020-06-03 13:07:37 +0900

    Add gatsby-node.js

---
+++ gatsby-node.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const _ = require("lodash");
+const moment = require("moment");
+const siteConfig = require("./data/SiteConfig");
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  let slug;
+  if (node.internal.type === "MarkdownRemark") {
+    const fileNode = getNode(node.parent);
+    const parsedFilePath = path.parse(fileNode.relativePath);
+    if (
+      Object.prototype.hasOwnProperty.call(node, "frontmatter") &&
+      Object.prototype.hasOwnProperty.call(node.frontmatter, "title")
+    ) {
+      slug = `/${_.kebabCase(node.frontmatter.title)}`;
+    } else if (parsedFilePath.name !== "index" && parsedFilePath.dir !== "") {
+      slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
+    } else if (parsedFilePath.dir === "") {
+      slug = `/${parsedFilePath.name}/`;
+    } else {
+      slug = `/${parsedFilePath.dir}/`;
+    }
+
+    if (Object.prototype.hasOwnProperty.call(node, "frontmatter")) {
+      if (Object.prototype.hasOwnProperty.call(node.frontmatter, "slug"))
+        slug = `/${_.kebabCase(node.frontmatter.slug)}`;
+      if (Object.prototype.hasOwnProperty.call(node.frontmatter, "date")) {
+        const date = moment(node.frontmatter.date, siteConfig.dateFromFormat);
+        if (!date.isValid)
+          console.warn(`WARNING: Invalid date.`, node.frontmatter);
+
+        createNodeField({ node, name: "date", value: date.toISOString() });
+      }
+    }
+    createNodeField({ node, name: "slug", value: slug });
+  }
+};
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+
+    const blogResult = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              tags
+              category
+              date
+            }
+          }
+        }
+      }
+    }
+  `);
+
+    const postsList = blogResult.data.allMarkdownRemark.edges;
+    const postTemplate = require.resolve('./src/templates/post.jsx');
+
+    postsList.forEach(edge => {
+        createPage({
+            type: 'Post',
+            match: '/blog:slug',
+            path: `/blog${edge.node.fields.slug}`,
+            component: postTemplate,
+            context: {
+                slug: edge.node.fields.slug,
+            },
+        })
+    })
+}
