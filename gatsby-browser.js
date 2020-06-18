d767903680d4fda39f2a1c1e9103782c9edaee2e
jam <nevvjann@gmail.com>
2020-06-18 12:41:32 +0900

    Add: intersectino observer polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 // gatsby-browser.js
 require("prism-themes/themes/prism-atom-dark.css")
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+
+// polyfill
+require('intersection-observer')
